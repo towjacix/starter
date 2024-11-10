84c0f6af21d29b89a807b0f310f5ef8680dd33ef
Manh Pham <54128221+towjacix@users.noreply.github.com>
2024-11-10 15:13:05 +0700

    CodeCompanion: add custom prompts

--- lua/configs/code_companion.lua
+++ lua/configs/code_companion.lua
@@ -1,3 +1,11 @@
+local fmt = string.format
+
+local constants = {
+  LLM_ROLE = "llm",
+  USER_ROLE = "user",
+  SYSTEM_ROLE = "system",
+}
+
 require("codecompanion").setup({
   adapters = {
     anthropic = function()
@@ -81,4 +89,427 @@ require("codecompanion").setup({
         return " (" .. tokens .. " tokens)"
       end,
     },
+
+ prompt_library = {
+    ["Custom Prompt"] = {
+      strategy = "inline",
+      description = "Prompt the LLM from Neovim",
+      opts = {
+        index = 3,
+        is_default = true,
+        is_slash_cmd = false,
+        user_prompt = true,
+      },
+      prompts = {
+        {
+          role = constants.SYSTEM_ROLE,
+          content = function(context)
+            return fmt(
+              [[I want you to act as a senior %s developer. I will ask you specific questions and I want you to return raw code only (no codeblocks and no explanations). If you can't respond with code, respond with nothing]],
+              context.filetype
+            )
+          end,
+          opts = {
+            visible = false,
+            tag = "system_tag",
+          },
+        },
+      },
+    },
+    ["Code workflow"] = {
+      strategy = "workflow",
+      description = "Use a workflow to guide an LLM in writing code",
+      opts = {
+        index = 4,
+        is_default = true,
+        short_name = "workflow",
+      },
+      prompts = {
+        {
+          -- We can group prompts together to make a workflow
+          -- This is the first prompt in the workflow
+          {
+            role = constants.SYSTEM_ROLE,
+            content = function(context)
+              return fmt(
+                "You carefully provide accurate, factual, thoughtful, nuanced answers, and are brilliant at reasoning. If you think there might not be a correct answer, you say so. Always spend a few sentences explaining background context, assumptions, and step-by-step thinking BEFORE you try to answer a question. Don't be verbose in your answers, but do provide details and examples where it might help the explanation. You are an expert software engineer for the %s language",
+                context.filetype
+              )
+            end,
+            opts = {
+              visible = false,
+            },
+          },
+          {
+            role = constants.USER_ROLE,
+            content = "I want you to ",
+            opts = {
+              auto_submit = false,
+            },
+          },
+        },
+        -- This is the second group of prompts
+        {
+          {
+            role = constants.USER_ROLE,
+            content = "Great. Now let's consider your code. I'd like you to check it carefully for correctness, style, and efficiency, and give constructive criticism for how to improve it.",
+            opts = {
+              auto_submit = false,
+            },
+          },
+        },
+        -- This is the final group of prompts
+        {
+          {
+            role = constants.USER_ROLE,
+            content = "Thanks. Now let's revise the code based on the feedback, without additional explanations.",
+            opts = {
+              auto_submit = false,
+            },
+          },
+        },
+      },
+    },
+    ["Explain"] = {
+      strategy = "chat",
+      description = "Explain how code in a buffer works",
+      opts = {
+        index = 5,
+        is_default = true,
+        is_slash_cmd = false,
+        modes = { "v" },
+        short_name = "explain",
+        auto_submit = true,
+        user_prompt = false,
+        stop_context_insertion = true,
+      },
+      prompts = {
+        {
+          role = constants.SYSTEM_ROLE,
+          content = [[When asked to explain code, follow these steps:
+
+1. Identify the programming language.
+2. Describe the purpose of the code and reference core concepts from the programming language.
+3. Explain each function or significant block of code, including parameters and return values.
+4. Highlight any specific functions or methods used and their roles.
+5. Provide context on how the code fits into a larger application if applicable.]],
+          opts = {
+            visible = false,
+          },
+        },
+        {
+          role = constants.USER_ROLE,
+          content = function(context)
+            local code = require("codecompanion.helpers.actions").get_code(context.start_line, context.end_line)
+
+            return fmt(
+              [[Please explain this code from buffer %d:
+
+```%s
+%s
+```
+]],
+              context.bufnr,
+              context.filetype,
+              code
+            )
+          end,
+          opts = {
+            contains_code = true,
+          },
+        },
+      },
+    },
+    ["Unit Tests"] = {
+      strategy = "chat",
+      description = "Generate unit tests for the selected code",
+      opts = {
+        index = 6,
+        is_default = true,
+        is_slash_cmd = false,
+        modes = { "v" },
+        short_name = "tests",
+        auto_submit = true,
+        user_prompt = false,
+        stop_context_insertion = true,
+      },
+      prompts = {
+        {
+          role = constants.SYSTEM_ROLE,
+          content = [[When generating unit tests, follow these steps:
+
+1. Identify the programming language.
+2. Identify the purpose of the function or module to be tested.
+3. List the edge cases and typical use cases that should be covered in the tests and share the plan with the user.
+4. Generate unit tests using an appropriate testing framework for the identified programming language.
+5. Ensure the tests cover:
+      - Normal cases
+      - Edge cases
+      - Error handling (if applicable)
+6. Provide the generated unit tests in a clear and organized manner without additional explanations or chat.]],
+          opts = {
+            visible = false,
+          },
+        },
+        {
+          role = constants.USER_ROLE,
+          content = function(context)
+            local code = require("codecompanion.helpers.actions").get_code(context.start_line, context.end_line)
+
+            return fmt(
+              [[Please generate unit tests for this code from buffer %d:
+
+```%s
+%s
+```
+]],
+              context.bufnr,
+              context.filetype,
+              code
+            )
+          end,
+          opts = {
+            contains_code = true,
+          },
+        },
+      },
+    },
+    ["Fix code"] = {
+      strategy = "chat",
+      description = "Fix the selected code",
+      opts = {
+        index = 7,
+        is_default = true,
+        is_slash_cmd = false,
+        modes = { "v" },
+        short_name = "fix",
+        auto_submit = true,
+        user_prompt = false,
+        stop_context_insertion = true,
+      },
+      prompts = {
+        {
+          role = constants.SYSTEM_ROLE,
+          content = [[When asked to fix code, follow these steps:
+
+1. **Identify the Issues**: Carefully read the provided code and identify any potential issues or improvements.
+2. **Plan the Fix**: Describe the plan for fixing the code in pseudocode, detailing each step.
+3. **Implement the Fix**: Write the corrected code in a single code block.
+4. **Explain the Fix**: Briefly explain what changes were made and why.
+
+Ensure the fixed code:
+
+- Includes necessary imports.
+- Handles potential errors.
+- Follows best practices for readability and maintainability.
+- Is formatted correctly.
+
+Use Markdown formatting and include the programming language name at the start of the code block.]],
+          opts = {
+            visible = false,
+          },
+        },
+        {
+          role = constants.USER_ROLE,
+          content = function(context)
+            local code = require("codecompanion.helpers.actions").get_code(context.start_line, context.end_line)
+
+            return fmt(
+              [[Please fix this code from buffer %d:
+
+```%s
+%s
+```
+]],
+              context.bufnr,
+              context.filetype,
+              code
+            )
+          end,
+          opts = {
+            contains_code = true,
+          },
+        },
+      },
+    },
+    ["Buffer selection"] = {
+      strategy = "inline",
+      description = "Send the current buffer to the LLM as part of an inline prompt",
+      opts = {
+        index = 8,
+        modes = { "v" },
+        is_default = true,
+        is_slash_cmd = false,
+        short_name = "buffer",
+        auto_submit = true,
+        user_prompt = true,
+        stop_context_insertion = true,
+      },
+      prompts = {
+        {
+          role = constants.SYSTEM_ROLE,
+          content = function(context)
+            return "I want you to act as a senior "
+              .. context.filetype
+              .. " developer. I will ask you specific questions and I want you to return raw code only (no codeblocks and no explanations). If you can't respond with code, respond with nothing."
+          end,
+          opts = {
+            visible = false,
+            tag = "system_tag",
+          },
+        },
+        {
+          role = constants.USER_ROLE,
+          content = function(context)
+            local buf_utils = require("codecompanion.utils.buffers")
+
+            return "```" .. context.filetype .. "\n" .. buf_utils.get_content(context.bufnr) .. "\n```\n\n"
+          end,
+          opts = {
+            contains_code = true,
+            visible = false,
+          },
+        },
+        {
+          role = constants.USER_ROLE,
+          condition = function(context)
+            return context.is_visual
+          end,
+          content = function(context)
+            local selection = require("codecompanion.helpers.actions").get_code(context.start_line, context.end_line)
+
+            return fmt(
+              [[And this is some code that relates to my question:
+
+```%s
+%s
+```
+]],
+              context.filetype,
+              selection
+            )
+          end,
+          opts = {
+            contains_code = true,
+            visible = true,
+            tag = "visual",
+          },
+        },
+      },
+    },
+    ["Explain LSP Diagnostics"] = {
+      strategy = "chat",
+      description = "Explain the LSP diagnostics for the selected code",
+      opts = {
+        index = 9,
+        is_default = true,
+        is_slash_cmd = false,
+        modes = { "v" },
+        short_name = "lsp",
+        auto_submit = true,
+        user_prompt = false,
+        stop_context_insertion = true,
+      },
+      prompts = {
+        {
+          role = constants.SYSTEM_ROLE,
+          content = [[You are an expert coder and helpful assistant who can help debug code diagnostics, such as warning and error messages. When appropriate, give solutions with code snippets as fenced codeblocks with a language identifier to enable syntax highlighting.]],
+          opts = {
+            visible = false,
+          },
+        },
+        {
+          role = constants.USER_ROLE,
+          content = function(context)
+            local diagnostics = require("codecompanion.helpers.actions").get_diagnostics(
+              context.start_line,
+              context.end_line,
+              context.bufnr
+            )
+
+            local concatenated_diagnostics = ""
+            for i, diagnostic in ipairs(diagnostics) do
+              concatenated_diagnostics = concatenated_diagnostics
+                .. i
+                .. ". Issue "
+                .. i
+                .. "\n  - Location: Line "
+                .. diagnostic.line_number
+                .. "\n  - Buffer: "
+                .. context.bufnr
+                .. "\n  - Severity: "
+                .. diagnostic.severity
+                .. "\n  - Message: "
+                .. diagnostic.message
+                .. "\n"
+            end
+
+            return fmt(
+              [[The programming language is %s. This is a list of the diagnostic messages:
+
+%s
+]],
+              context.filetype,
+              concatenated_diagnostics
+            )
+          end,
+        },
+        {
+          role = constants.USER_ROLE,
+          content = function(context)
+            local code = require("codecompanion.helpers.actions").get_code(
+              context.start_line,
+              context.end_line,
+              { show_line_numbers = true }
+            )
+            return fmt(
+              [[
+This is the code, for context:
+
+```%s
+%s
+```
+]],
+              context.filetype,
+              code
+            )
+          end,
+          opts = {
+            contains_code = true,
+          },
+        },
+      },
+    },
+    ["Generate a Commit Message"] = {
+      strategy = "chat",
+      description = "Generate a commit message",
+      opts = {
+        index = 10,
+        is_default = true,
+        is_slash_cmd = true,
+        short_name = "commit",
+        auto_submit = true,
+      },
+      prompts = {
+        {
+          role = constants.USER_ROLE,
+          content = function()
+            return fmt(
+              [[You are an expert at following the Conventional Commit specification. Given the git diff listed below, please generate a commit message for me:
+
+```diff
+%s
+```
+]],
+              vim.fn.system("git diff --no-ext-diff --staged")
+            )
+          end,
+          opts = {
+            contains_code = true,
+          },
+        },
+      },
+    },
+  },
+
 })
