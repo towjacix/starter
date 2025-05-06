8302d383c655c0841faff65eb3afddd0ea6b7409
Manh Pham <54128221+towjacix@users.noreply.github.com>
2025-05-06 22:11:57 +0700

    idk

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -6,3 +6,19 @@ local servers = {
 }
 
 vim.lsp.enable(servers)
+
+-- config table
+vim.lsp.config("pylsp", {
+    settings = {
+        ["pylsp"] = {
+            plugins = {
+                rope_completion = {enabled = true, eager = true},
+                pycodestyle = {
+                    enabled = true,
+                    ignore = {"W391", "E111", "W291", "E501"}, -- Added E111 and W291
+                    indentSize = 4
+                }
+            }
+        }
+    }
+})
